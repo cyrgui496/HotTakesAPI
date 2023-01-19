1fec3967ede333a1ee563f1939011a0d95c1fb38
cyrgui496 <cyrgui496@gmail.com>
2023-01-19 12:50:33 +0100

    add midlleware is-owner

---
+++ middleware/is-owner.js
@@ -0,0 +1,22 @@
+const Sauce = require('../models/Sauces')
+
+module.exports = (req, res, next) => {
+    Sauce.findOne({
+            _id: req.params.id
+        })
+        .then((sauce) => {
+            if (sauce.userId != req.auth.userId) {
+                res.status(401).json({
+                    message: 'Not authorized'
+                })
+            } else {
+                req.sauce = sauce
+                next()
+            }
+        })
+        .catch((error) => {
+            res.status(400).json({
+                error
+            })
+        })
+}
