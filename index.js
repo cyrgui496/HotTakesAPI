6aafcf5758f793a8592d4f69674af872a64ad658
cyrgui496 <cyrgui496@gmail.com>
2023-01-23 23:41:42 +0100

    add comment at user.js in routes

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
-const express = require("express")
-const cors = require('cors')
-const path = require('path');
-const dotenv = require('dotenv')
-const mongoose = require("mongoose")
+const express = require("express") // standard framework for server development in Node.js
+const cors = require('cors') // CORS is a node.js package that can be used to enable CORS
+const path = require('path') // The Path module provides a way of working with directories and file paths
+const dotenv = require('dotenv') // Dotenv is a zero-dependency module that loads environment variables from a .env file into process.env.
+const mongoose = require("mongoose") // Mongoose is a MongoDB object modeling tool
+// inclusion of routes
 const userRoutes = require('./routes/user')
 const sauceRoutes = require('./routes/sauce')
 
@@ -12,13 +13,15 @@ mongoose.connect(`mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD
         .then(() => console.log("Connected at MongoDB"))
         .catch((err) => console.log("Not connected at MongoDB", err))
 
-const app = express()
+const app = express() // We create the express app
 
-app.use(express.json())
+// use middleware
+app.use(express.json()) // The express.json() function is a built-in middleware function in Express. It parses incoming requests with JSON payloads.
 app.use(cors())
-
+// use routes
 app.use('/api/auth', userRoutes)
 app.use('/api', sauceRoutes)
 app.use('/images', express.static(path.join(__dirname, 'images')));
 
+// We run the app on port 3000
 app.listen(3000)
--- routes/user.js
+++ routes/user.js
@@ -1,9 +1,12 @@
-const express = require('express')
-const router = express.Router()
+const express = require('express') // standard framework for server development in Node.js
+const router = express.Router() // to create modular, mountable route handlers with midlleware express.router
 
+// inclusion of user controller
 const userCtrl = require('../controllers/user')
 
+// CRUD operation
 router.post('/signup', userCtrl.signup)
 router.post('/login', userCtrl.login)
 
+// We export const router
 module.exports = router
