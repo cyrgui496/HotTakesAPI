ce490c5c9dddbbb60bd7aa1e8d916ba961fda168
cyrgui496 <cyrgui496@gmail.com>
2023-01-23 23:08:26 +0100

    add comment to sauces.js in routes

--- routes/sauce.js
+++ routes/sauce.js
@@ -1,12 +1,15 @@
-const express = require("express")
-const router = express.Router()
+const express = require("express") // standard framework for server development in Node.js
+const router = express.Router() // to create modular, mountable route handlers with midlleware express.router
 
+//inclusion of middleware
 const auth = require('../middleware/auth')
 const multer = require('../middleware/multer-config')
 const isOwner = require('../middleware/is-owner')
 
+// inclusion of sauce controller
 const sauceCtrl = require('../controllers/sauce')
 
+// CRUD operation
 router.post('/sauces', auth, multer, sauceCtrl.createSauce)
 router.get('/sauces', auth, sauceCtrl.getAllSauces)
 router.get('/sauces/:id', auth, sauceCtrl.getOneSauce)
@@ -14,4 +17,5 @@ router.put('/sauces/:id', auth, isOwner, multer, sauceCtrl.modifySauce)
 router.delete('/sauces/:id', auth, isOwner, sauceCtrl.deleteSauce)
 router.post('/sauces/:id/like', auth, sauceCtrl.likeSauce)
 
+// We export const router
 module.exports = router
